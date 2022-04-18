0d93c6f9716f973ebcb0621b31204743c35a32bd
TanushriChatterjee <tanushri1595@gmail.com>
2022-04-18 15:18:16 +0530

    started redux

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,16 +19,16 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 //Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
 
-
-    const resultPerPage = 5;
-    const productCount = await Product.countDocuments();
+    const resultPerPage = 8;
+    const productsCount = await Product.countDocuments();
 
     const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
 
-    const products = await apiFeature.query;
+    let products = await apiFeature.query;
     res.status(200).json({
         success: true,
-        products
+        products,
+        productsCount
     })
 
 });
@@ -45,7 +45,7 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         success: true,
         product,
-        productCount
+      
     })
 });
 
---
+++ front-end/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import Header from "./component/layout/Header/Header.jsx";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+import Footer from "./component/layout/Footer/Footer.jsx";
+import Home from "./component/Home/Home.jsx";
+
+
+function App() {
+
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
+  return (
+    
+    <Router>
+      <Header />
+      <Routes>
+     <Route exact path="/" element={<Home />} />
+     
+     </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
